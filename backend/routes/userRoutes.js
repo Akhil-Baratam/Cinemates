16632b47718ab1f8ce9ff4f56babc585f8872888
Akhil Baratam <thecosmicthinkerab@gmail.com>
2024-06-24 21:25:10 +0530

    AfterUser

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const protectRoute = require('../middleware/protectRoute');
+const { getUserProfile, followUnfollowUser, getSuggestedUsers, updateUser } = require('../controllers/userController');
+
+const router = express.Router();
+
+router.get("/profile/:username", protectRoute, getUserProfile);
+router.post("/follow/:id", protectRoute , followUnfollowUser);
+router.get("/suggested", protectRoute , getSuggestedUsers);
+router.post("/update", protectRoute , updateUser);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/api/auth', require("./routes/authRoutes"));
-// app.use('/api/users', require("./routes/userRoutes"));
+app.use('/api/users', require("./routes/userRoutes"));
 // app.use('/api/posts', require("./routes/postRoutes"));
 // app.use('/api/notifications', require("./routes/notificationRoutes"));
 
